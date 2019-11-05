f190b38d33d02b3b7c8e394cf1c841e7fb509f53
LichengBao0127 <55646305+LichengBao0127@users.noreply.github.com>
2019-11-04 23:38:34 -0800

    Jerry

---
+++ ParkinSense/ViewController/BubbleUIViewController.swift
@@ -0,0 +1,92 @@
+//-----------------------------------------------------------------
+//  File: GameTwoViewController.swift
+//
+//  Team: ParkinSense - PDD Inc.
+//
+//  Programmer(s): Higgins Weng, Licheng Bao (Jerry)
+//
+//  Changes:
+//      - list change here
+//
+//  Known Bugs:
+//      - list known bugs here
+//
+//-----------------------------------------------------------------
+
+import UIKit
+import AVFoundation
+
+class BubbleUIViewController: UIViewController {
+    
+  
+    
+    @IBOutlet weak var BubbleStartGame: UIButton!
+    @IBOutlet weak var BubbleQuitGame: UIButton!
+    var soundEffect: AVAudioPlayer = AVAudioPlayer()
+    
+    
+    /**
+     Loads the view on the screen - adds buttons and sound toggle
+     
+     - Returns: None
+     **/
+    @IBAction func ssss(_ sender: Any) {
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.BubbleStartGame.layer.cornerRadius = 25 // Rounded Start Button
+        self.BubbleQuitGame.layer.cornerRadius = 25 // Rounded Quit Button
+        
+        guard let musicFile = Bundle.main.path(forResource: "Roots", ofType: ".mp3") else {
+            print("File Not Found")
+            return
+        }
+        
+        do {
+            try soundEffect = AVAudioPlayer(contentsOf: URL(fileURLWithPath: musicFile))
+            soundEffect.numberOfLoops = -1
+        }
+        catch {
+            print(error)
+        }
+        
+        soundEffect.play()
+    }
+    
+    
+    /**
+     Sets sound to be on/off based on the toggle
+     
+     - Returns: None
+     **/
+    @IBAction func soundToggle(_ sender: UISwitch) {
+        if (sender.isOn == false)
+        {
+            soundEffect.pause()
+        }
+        else{
+            soundEffect.play()
+        }
+    }
+    
+    
+    /**
+     Stops the sound from playing
+     
+     - Returns: None
+     **/
+    @IBAction func stopSound(_ sender: Any) {
+        soundEffect.stop()
+    }
+    
+    
+    /**
+     Starts the game Bubble pop
+     
+     - Returns: None
+     **/
+    @IBAction func startTheGame(_ sender: Any) {
+    }
+    
+}
---
+++ ParkinSense/ViewController/BubbleViewController.swift
@@ -0,0 +1,9 @@
+//
+//  BubbleViewController.swift
+//  ParkinSense
+//
+//  Created by 包立诚 on 2019/11/4.
+//  Copyright © 2019 PDD Inc. All rights reserved.
+//
+
+import Foundation
